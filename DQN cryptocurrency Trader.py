d7071d1416b989480da0acbeaf5184d1fb8fb5dd
Mohammad Asadolahi <79268727+MohammadAsadolahi@users.noreply.github.com>
2023-06-16 10:26:42 +0330

    Update DQN cryptocurrency Trader.py
    plots meta data has been added

--- DQN cryptocurrency Trader.py
+++ DQN cryptocurrency Trader.py
@@ -14,13 +14,19 @@ import yfinance as yf
 
 
 Crypto_name = ["BTC-USD"] # replace with other crypto currency e.g. "ETH-USD" 'XRP-USD' "LTC-USD"
+start_date="2022-06-20"
+end_date='2023-06-20'
 closing_price=pd.DataFrame()
 for i in Crypto_name:
     data= yf.Ticker(i)
-    data = data.history(start="2022-06-20" , end='2023-06-20',interval="1h")
+    data = data.history(start=start_date , end=end_date,interval="1h")
     colse=pd.DataFrame(data.Close)
     closing_price[i] = colse
  
 
 # plot the closing price changes in the given period
+plt.xlabel("date")
+plt.ylabel("closing price")
+plt.title(f"bitcoin closing prices from{start_date} to {end_date}")
 plt.plot(closing_price['BTC-USD'])
+
